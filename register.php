af081a3d279e09a325908deaf8a2df4bd3efaf39
liang799 <neohtp@tutanota.com>
2022-01-23 21:00:44 +0800

    inserting record to sql WIP

--- register.php
+++ register.php
@@ -8,13 +8,16 @@ $username_err = $password_err = $email_err = "";
  
 // Processing form data when form is submitted
 if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+	// Username textfield
 	if (empty(trim($_POST["username"]))) {
 	    $username_err = "username cannot be empty";
 	} elseif(!preg_match('/^[a-zA-Z0-9_]+$/', trim($_POST["username"]))){
 		$username = $_POST["username"];
 		$username_err = "Username can only contain letters, numbers, and underscores.";
     } else {
-		$stmt = $conn->prepare("SELECT * FROM websitedatabase.accounts WHERE userName=?"); //prepared statements
+		//prepared statement is better than "WHERE userName=$var" (unprepared statement)
+		$stmt = $conn->prepare("SELECT * FROM websitedatabase.accounts WHERE userName=?"); 
 		$stmt->bind_param("s", $_POST["username"]);
 		$stmt->execute();
 		$result = $stmt->get_result();
@@ -26,17 +29,40 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 		$stmt->close();
 	}
 
-
+	// Email textfield
 	if (empty(trim($_POST["email"]))) {
 		$email_err = "email cannot be empty";
 	} elseif (!preg_match("/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/ix", $_POST["email"])) {
 		$email_err = "Not a valid email";
+		$email = $_POST["email"];
 	} else {
-
+		$email = $_POST["email"];
 	}
 
-    
+	// Password textfield
+	if (empty($_POST["password"])) {
+		$password_err = "Password cannot be empty";
+    } elseif(strlen($_POST["password"]) < 6){
+        $password_err = "Password must have at least 6 characters.";
+		$password = $_POST["password"];
+	} else {
+		$password = $_POST["password"];
+		$encrypted = password_hash($password, PASSWORD_DEFAULT);
+		// don't need to manually add in salt and use hash function
+	}
 
+	// Check for no error messages
+	if (empty($username_err) && empty($email_err) && empty($password_err)) {
+		/* Prepared statement, stage 1: prepare */
+		$stmt = $conn->prepare("INSERT INTO accounts(userName, userEmail, userPassword) VALUES (?, ?, ?)");
+
+		/* Prepared statement, stage 2: bind and execute */
+		$stmt->bind_param("sss", $username, $email, $encrypted); 
+		$stmt->execute(); 
+		echo "inserted $username $email $encrypted";
+		$stmt->close();
+
+	}
 }
 ?>
  
