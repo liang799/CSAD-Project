2f8f209c49b3b90ff6077832d77d2eccfdb8e25b
liang799 <neohtp@tutanota.com>
2022-01-28 23:53:55 +0800

    added date in user creation

--- include/functions.php
+++ include/functions.php
@@ -22,3 +22,49 @@ function checkPOST($rows, $col, &$var, $postVar, &$errorMsg, $error) {
 	else
 		$errorMsg = $error;
 }
+
+function alert($color, $msg) {
+	switch ($color) {
+		case 'red':
+			$hex = '#f44336;';
+			break;
+		case 'green':
+			$hex = '#2eb885;';
+			break;
+		default:
+			$hex = '#46a7f5';
+			break;
+	}
+	echo '<style>
+	 /* The alert message box */
+	.alert {
+	  padding: 20px;
+	  background-color: ' . $hex . ';
+	  color: white;
+	  margin-bottom: 15px;
+	}
+
+	/* The close button */
+	.closebtn {
+	  margin-left: 15px;
+	  color: white;
+	  font-weight: bold;
+	  float: right;
+	  font-size: 22px;
+	  line-height: 20px;
+	  cursor: pointer;
+	  transition: 0.3s;
+	}
+
+	/* When moving the mouse over the close button */
+	.closebtn:hover {
+	  color: black;
+	} 
+	</style>
+
+	<div class="alert">
+		<span class="closebtn" onclick="this.parentElement.style.display=\'none\';">&times;</span> ' . 
+		$msg . 
+	'</div>  ';
+}
+?>
