aaa9dc52ba60b883695969aee4c3c50c65006120
liang799 <neohtp@tutanota.com>
2022-01-29 21:26:20 +0800

    cleaned up code

---
+++ include/checkAuth.php
@@ -0,0 +1,22 @@
+<?php
+
+$row = [];
+
+if (empty($_COOKIE['auth']) or empty($_COOKIE['username']) or empty($_COOKIE['password'])) {
+	header("location: index.php");
+} elseif ($_COOKIE['auth'] == false) {
+	header("location: register.php");
+} else {
+	// try getting row containing user
+	$result = selectUser($conn, $_COOKIE['username']);
+	$row = $result->fetch_all(MYSQLI_ASSOC);
+	if (empty($row)) {
+		header("location: index.php");
+	} else {
+		$encrypted = $row[0]['userPassword'];
+		if ($encrypted != $_COOKIE['password'])
+			header("location: index.php");
+	}
+}
+
+?>
--- include/home.php
+++ include/home.php
@@ -2,23 +2,6 @@
 
 require_once "config.php";
 require_once "functions.php";
-$row = [];
-
-if (empty($_COOKIE['auth']) or empty($_COOKIE['username']) or empty($_COOKIE['password'])) {
-	header("location: index.php");
-} elseif ($_COOKIE['auth'] == false) {
-	header("location: register.php");
-} else {
-	// try getting row containing user
-	$result = selectUser($conn, $_COOKIE['username']);
-	$row = $result->fetch_all(MYSQLI_ASSOC);
-	if (empty($row)) {
-		header("location: index.php");
-	} else {
-		$encrypted = $row[0]['userPassword'];
-		if ($encrypted != $_COOKIE['password'])
-			header("location: index.php");
-	}
-}
+require_once "checkAuth.php";
  
 ?>
