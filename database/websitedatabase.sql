437258e638b22fd4b63319fe02bdd77bf104b1d2
TheTreeAnt <TheTreeAnt@gmail.com>
2022-02-14 20:27:06 +0800

    Updated database due to bug

--- database/websitedatabase.sql
+++ database/websitedatabase.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Feb 14, 2022 at 10:54 AM
+-- Generation Time: Feb 14, 2022 at 12:26 PM
 -- Server version: 5.7.31
 -- PHP Version: 7.3.21
 
