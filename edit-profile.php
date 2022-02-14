f39584dd662491e0f876df93b171067d3aef30a5
liang799 <neohtp@tutanota.com>
2022-02-14 21:26:37 +0800

    managed to upload picture to database

--- edit-profile.php
+++ edit-profile.php
@@ -8,8 +8,15 @@ $result = selectUser($conn, $user);
 $row = $result->fetch_all(MYSQLI_ASSOC);
 $date = $row[0]['userCreateDate'];
 
-if($_SERVER["REQUEST_METHOD"] == "POST"){
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    $image = $_FILES['image']['tmp_name']['0'];
+    $img = file_get_contents($image);
 
+	$stmt = $conn->prepare("UPDATE accounts SET userPicture = ? WHERE userName = ?"); 
+
+	$stmt->bind_param("ss", $img, $user);
+	$stmt->execute();
+	$stmt->close();
 }
 ?>
 
@@ -25,7 +32,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
 	</head>
 	<body oncontextmenu='return false' class='snippet-body'>
-		<div class="container rounded bg-white mt-5 mb-5">
+		<form method="POST" action="" class="container rounded bg-white mt-5 mb-5" enctype="multipart/form-data">
 			<div class="row">
 					<div class="col-md-3 border-right">
 						<div class="d-flex flex-column align-items-center text-center p-3 py-5">
@@ -34,7 +41,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 							    <i class="bi bi-camera">&nbsp;</i>
 								<span>Change Image</span>
 							  </label>
-							  <input id="file" type="file" onchange="loadFile(event)"/>
+							  <input id="file" type="file" name="image[]" onchange="loadFile(event)"/>
 							  <img src="assets/img/user/default.png" id="output" width="200" />
 							</div>
 							<span style="padding-top: 15px" class="font-weight-bold"><?php echo $user ?></span>
@@ -46,7 +53,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 							<div class="d-flex justify-content-between align-items-center mb-3">
 								<h4>Profile Settings</h4>
 							</div>
-							<form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
 								<div class="row mt-2">
 									<div class="col-md-6">
 										<label>Username</label>
@@ -66,11 +72,10 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 										<input type="submit" class="btn btn-primary" value="Submit">
 									</div>
 								</div>
-							</form>
 						</div>
 					</div>
 			</div>
-		</div>
+		</form>
 		<script type='text/javascript' src='https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/js/bootstrap.bundle.min.js'></script>
 	</body>
 </html>
