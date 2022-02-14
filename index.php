da6f1cdf9fac20c40d67a4b43841c4e0a5a2a254
josep <josep@LAPTOP-54MVQQUP>
2022-02-14 22:04:45 +0800

    i think i added submit button modal, just that cant access for now

--- index.php
+++ index.php
@@ -47,6 +47,72 @@
 			.paypal-button-text{
 				color:white!important;
 			}
+                        .rating {
+                            display: inline-block;
+                            position: relative;
+                            height: 50px;
+                            line-height: 50px;
+                            font-size: 50px;
+                        }
+
+                        .rating label {
+                          position: absolute;
+                          top: 0;
+                          left: 0;
+                          height: 100%;
+                          cursor: pointer;
+                        }
+
+                        .rating label:last-child {
+                          position: static;
+                        }
+
+                        .rating label:nth-child(1) {
+                          z-index: 5;
+                        }
+
+                        .rating label:nth-child(2) {
+                          z-index: 4;
+                        }
+
+                        .rating label:nth-child(3) {
+                          z-index: 3;
+                        }
+
+                        .rating label:nth-child(4) {
+                          z-index: 2;
+                        }
+
+                        .rating label:nth-child(5) {
+                          z-index: 1;
+                        }
+
+                        .rating label input {
+                          position: absolute;
+                          top: 0;
+                          left: 0;
+                          opacity: 0;
+                        }
+
+                        .rating label .icon {
+                          float: left;
+                          color: transparent;
+                        }
+
+                        .rating label:last-child .icon {
+                          color: #000;
+                        }
+
+                        .rating:not(:hover) label input:checked ~ .icon,
+                        .rating:hover label:hover input ~ .icon {
+                          color: #78BE21;
+                        }
+
+                        .rating label input:focus:not(:checked) ~ .icon:last-child {
+                          color: #000;
+                          text-shadow: 0 0 5px #09f;
+                        }
+                        
 			<?php 
 				if (!$contact_success) {
 					echo '
@@ -293,7 +359,7 @@
                                 </div>
                             </div>
                             <!-- Submit Button-->
-                            <div class="d-grid"><button class="btn btn-primary btn-xl" id="submitButton" type="submit">Submit</button></div>
+                            <div class="d-grid"><button class="btn btn-primary btn-xl" id="submitButton" type="submit" data-bs-toggle="modal" data-bs-target="#submitModal">Submit</button></div>
                         </form>
                     </div>
                 </div>
@@ -329,7 +395,51 @@
 			  </div>
 			</div>
 		</div>
-
+                <!---- Submit Modal ---->
+		<div class="modal fade" id="submitModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="submitModal" aria-hidden="true">
+                    <div class="modal-dialog">
+                      <div class="modal-content">
+                            <div class="modal-header">
+                              <h5 class="modal-title" id="exampleModalLabel">Rate Us!</h5>
+                              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                <form class="rating">
+                                    <label>
+                                      <input type="radio" name="stars" value="1" />
+                                      <span class="icon">★</span>
+                                    </label>
+                                    <label>
+                                      <input type="radio" name="stars" value="2" />
+                                      <span class="icon">★</span>
+                                      <span class="icon">★</span>
+                                    </label>
+                                    <label>
+                                      <input type="radio" name="stars" value="3" />
+                                      <span class="icon">★</span>
+                                      <span class="icon">★</span>
+                                      <span class="icon">★</span>   
+                                    </label>
+                                    <label>
+                                      <input type="radio" name="stars" value="4" />
+                                      <span class="icon">★</span>
+                                      <span class="icon">★</span>
+                                      <span class="icon">★</span>
+                                      <span class="icon">★</span>
+                                    </label>
+                                    <label>
+                                      <input type="radio" name="stars" value="5" />
+                                      <span class="icon">★</span>
+                                      <span class="icon">★</span>
+                                      <span class="icon">★</span>
+                                      <span class="icon">★</span>
+                                      <span class="icon">★</span>
+                                    </label>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 		<!---- Login Modal ---->
 		<div class="modal fade" id="loginModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="loginModal" aria-hidden="true">
 			<div class="modal-dialog">
